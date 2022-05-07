e1edfb29382ee80f21485130d8831b7bcbdecc90
Vineet Pandey <vineet1992.vp@gmail.com>
2022-05-07 20:13:31 +0530

    add biconsumer code example

--- java_8_concpet/src/lamda/LambdaExample.java
+++ java_8_concpet/src/lamda/LambdaExample.java
@@ -1,7 +1,11 @@
 package lamda;
 
+import java.util.function.BiConsumer;
+
 public class LambdaExample {
     public static void main(String[] args) {
+        BiConsumer<Integer, Integer> biConsumer=(a,b)-> System.out.println(a+b);
+        biConsumer.accept(10,5);
         
     }
 }
